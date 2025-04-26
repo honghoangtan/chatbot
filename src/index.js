7b3824dfd66a825976c26468084c4536430261ad
gld-tanhong <tanhong@golivedev.com>
2025-04-26 23:56:27 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngine';  
+import webRoutes from './routes/web';
+import dotenv   from 'dotenv';
+
+let app = express();
+
+dotenv.config(); // load env variables
+
+// config view engine
+viewEngine(app);
+
+webRoutes(app)
+
+// config body parser
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// init web routes
+
+
+
+let port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Backend Nodejs is running on the port: " + port);
+})
